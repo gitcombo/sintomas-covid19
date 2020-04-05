4b47762ad48024312324fe9ff3ac1676c7a12b05
André Morales <landremorales@gmail.com>
2020-04-05 12:44:36 -0600

    add header app.js

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express     = require('express');
 const app         = express();
 const sintomasRoutes = require('./api/sintomas-routes');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
 app.use('/sintomas', sintomasRoutes);
 
-// Manejador de errores
+
+
 app.use((req, res, next) => {
     const error = new Error('Recurso no encontrado. COVID19 viene por ti.');
     error.status = 404;
